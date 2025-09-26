5450f8c7b98c80488a8d293ff8702b474856746d
Skyysur <skyysur@gmail.com>
2025-09-26 10:26:42 -0700

    Delete skysur-portfolio directory

--- skysur-portfolio/src/portfolio/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-export default function Button({ asChild = false, variant='primary', className='', children, ...props }) {
-  const cls = ['btn', variant==='outline' ? 'btn-outline' : 'btn-primary', className].join(' ')
-  if (asChild) return <a className={cls} {...props}>{children}</a>
-  return <button className={cls} {...props}>{children}</button>
-}
